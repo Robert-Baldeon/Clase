83a33af84b314bdf356842c8a090dfcfc63ad44b
Robert-Baldeon <257325853+Robert-Baldeon@users.noreply.github.com>
2026-01-27 13:39:44 +0100

    update entity Consulta exercise 2 jpa

--- src/main/java/com/example/jpa/ejercicio_02/entities/Consulta.java
+++ src/main/java/com/example/jpa/ejercicio_02/entities/Consulta.java
@@ -1,8 +1,40 @@
 package com.example.jpa.ejercicio_02.entities;
 
-public class Consulta {
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "consulta")
+public class Consulta implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "motivo")
     private String motivo;
+
+    @Column(name = "id_veterinario")
+    private Veterinario veterinario;
+
+    private List<Mascota> pacientes;
+
+    public Consulta() {}
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getMotivo() { return motivo; }
+    public void setMotivo(String motivo) { this.motivo = motivo; }
+    public Veterinario getVeterinario() { return veterinario; }
+    public void setVeterinario(Veterinario veterinario) { this.veterinario = veterinario; }
+    public List<Mascota> getPacientes() { return pacientes; }
+    public void setPacientes(List<Mascota> pacientes) { this.pacientes = pacientes; }
 }
