b58fc08401c41feeac9a6cd34770af09b95d265d
Robert-Baldeon <257325853+Robert-Baldeon@users.noreply.github.com>
2026-01-27 13:38:00 +0100

    update entity Mascota exercise 2 jpa

--- src/main/java/com/example/jpa/ejercicio_02/entities/Mascota.java
+++ src/main/java/com/example/jpa/ejercicio_02/entities/Mascota.java
@@ -1,12 +1,60 @@
 package com.example.jpa.ejercicio_02.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "mascota")
 public class Mascota implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer  id;
+
+    @Column(name = "nombre")
     private String nombre;
-    private String especia;
+
+    @Column(name = "especie")
+    private String especie;
+
+    @OneToOne
+    @JoinColumn(name = "id_historial")
     private HistorialMedico historialMedico;
+
+    @ManyToMany
+    @JoinTable(
+    name = "registro_consultas",
+    joinColumns = @JoinColumn(name = "id_mascota"),
+    inverseJoinColumns = @JoinColumn(name = "id_consulta")
+    )
+    private List<Consulta> consultas;
+
+    public Mascota() {}
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getEspecie() { return especie; }
+    public void setEspecie(String especie) { this.especie = especie; }
+
+    public HistorialMedico getHistorialMedico() { return historialMedico; }
+    public void setHistorialMedico(HistorialMedico historialMedico) { this.historialMedico = historialMedico; }
+
+    public List<Consulta> getConsultas() { return consultas; }
+    public void setConsultas(List<Consulta> consultas) { this.consultas = consultas; }
 }
