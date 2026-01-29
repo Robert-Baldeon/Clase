9d0012bfcfad7cf75e920ed1d3e854a7f310f98d
Robert-Baldeon <257325853+Robert-Baldeon@users.noreply.github.com>
2026-01-29 13:22:50 +0100

    add emf for exercise 7 jpa

---
+++ src/main/java/com/example/jpa/ejercicio_07/util/EMF.java
@@ -0,0 +1,27 @@
+package com.example.jpa.ejercicio_07.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EMF {
+    private static final String PERSISTENCE_UNIT_NAME = "UnidadAcademia";
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static EntityManager getEntityManager() {
+        if (entityManagerFactory == null) {
+            try {
+                entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            } catch (Exception e) {
+                System.err.println("Error al inicializar el EntityManagerFactory");
+            }
+        }
+        return entityManagerFactory.createEntityManager();
+    }
+
+    public static void close() {
+        if (entityManagerFactory != null && entityManagerFactory.isOpen()) {
+            entityManagerFactory.close();
+        }
+    }
+}
