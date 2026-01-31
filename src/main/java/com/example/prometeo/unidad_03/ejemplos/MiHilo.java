49937b8ae1bfded298f679a4b5b85c8b54f7ff43
Robert-Baldeon <257325853+Robert-Baldeon@users.noreply.github.com>
2026-01-31 15:42:00 +0100

    add ejemplos of prometeo ejemplos

---
+++ src/main/java/com/example/prometeo/unidad_03/ejemplos/MiHilo.java
@@ -0,0 +1,12 @@
+package com.example.prometeo.unidad_03.ejemplos;
+
+public class MiHilo extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Ejecutando en hilo: " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        new MiHilo().start();
+    }
+}
---
+++ src/main/java/com/example/prometeo/unidad_03/ejemplos/MiTarea.java
@@ -0,0 +1,13 @@
+package com.example.prometeo.unidad_03.ejemplos;
+
+public class MiTarea implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Ejecutando en hilo: " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MiTarea(), "Hilo-Tarea");
+        thread.start();
+    }
+}
